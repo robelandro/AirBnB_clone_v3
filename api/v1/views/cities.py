907ace0f5117e47ad011a77ef0152bc93cfddc5e
meron <104758118+merraa@users.noreply.github.com>
2023-01-23 16:55:50 +0300

    Update cities.py

--- api/v1/views/cities.py
+++ api/v1/views/cities.py
@@ -1,77 +1,56 @@
 #!/usr/bin/python3
-"""cities.py"""
-
+"""
+    This is the cities page handler for Flask.
+"""
 from api.v1.views import app_views
-from flask import abort, jsonify, make_response, request
 from models import storage
+from flask import abort, jsonify, request
+
 from models.city import City
 from models.state import State
 
 
-@app_views.route('/states/<string:state_id>/cities', methods=['GET'],
-                 strict_slashes=False)
-def get_cities(state_id):
-    """get city information for all cities in a specified state"""
-    state = storage.get("State", state_id)
-    if state is None:
-        abort(404)
-    cities = []
-    for city in state.cities:
-        cities.append(city.to_dict())
-    return jsonify(cities)
+@app_views.route('/states/<id>/cities', methods=['GET', 'POST'])
+def states_id_cities(id):
+    """
+        Flask route at /states/<id>/cities.
+    """
+    state = storage.get(State, id)
+    if (state):
+        if request.method == 'POST':
+            kwargs = request.get_json()
+            if not kwargs:
+                return {"error": "Not a JSON"}, 400
+            if "name" not in kwargs:
+                return {"error": "Missing name"}, 400
+            new_city = City(state_id=id, **kwargs)
+            new_city.save()
+            return new_city.to_dict(), 201
+
+        elif request.method == 'GET':
+            return jsonify([c.to_dict() for c in state.cities])
+    abort(404)
 
 
-@app_views.route('/cities/<string:city_id>', methods=['GET'],
-                 strict_slashes=False)
-def get_city(city_id):
-    """get city information for specified city"""
-    city = storage.get("City", city_id)
-    if city is None:
-        abort(404)
-    return jsonify(city.to_dict())
+@app_views.route('/cities/<id>', methods=['GET', 'DELETE', 'PUT'])
+def cities_id(id):
+    """
+        Flask route at /cities/<id>.
+    """
+    city = storage.get(City, id)
+    if (city):
+        if request.method == 'DELETE':
+            city.delete()
+            storage.save()
+            return {}, 200
 
-
-@app_views.route('/cities/<string:city_id>', methods=['DELETE'],
-                 strict_slashes=False)
-def delete_city(city_id):
-    """deletes a city based on its city_id"""
-    city = storage.get("City", city_id)
-    if city is None:
-        abort(404)
-    city.delete()
-    storage.save()
-    return (jsonify({}))
-
-
-@app_views.route('/states/<string:state_id>/cities/', methods=['POST'],
-                 strict_slashes=False)
-def post_city(state_id):
-    """create a new city"""
-    state = storage.get("State", state_id)
-    if state is None:
-        abort(404)
-    if not request.get_json():
-        return make_response(jsonify({'error': 'Not a JSON'}), 400)
-    if 'name' not in request.get_json():
-        return make_response(jsonify({'error': 'Missing name'}), 400)
-    kwargs = request.get_json()
-    kwargs['state_id'] = state_id
-    city = City(**kwargs)
-    city.save()
-    return make_response(jsonify(city.to_dict()), 201)
-
-
-@app_views.route('/cities/<string:city_id>', methods=['PUT'],
-                 strict_slashes=False)
-def put_city(city_id):
-    """update a city"""
-    city = storage.get("City", city_id)
-    if city is None:
-        abort(404)
-    if not request.get_json():
-        return make_response(jsonify({'error': 'Not a JSON'}), 400)
-    for attr, val in request.get_json().items():
-        if attr not in ['id', 'state_id', 'created_at', 'updated_at']:
-            setattr(city, attr, val)
-    city.save()
-    return jsonify(city.to_dict())
+        elif request.method == 'PUT':
+            kwargs = request.get_json()
+            if not kwargs:
+                return {"error": "Not a JSON"}, 400
+            for k, v in kwargs.items():
+                if k not in ["id", "state_id", "created_at", "updated_at"]:
+                    setattr(city, k, v)
+            city.save()
+        return city.to_dict()
+    abort(404)
